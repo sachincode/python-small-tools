3d2e32494006a1b9802e6294af6cb11df1acee7f
shicheng.zhang <shicheng.zhang@qunar.com>
2017-03-10 14:38:22 +0800

    doupo reconsitution

--- spider/novel/doupo.py
+++ spider/novel/doupo.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
     《斗破苍穹》阅读器
+    chapter_id: 2 - 1646
 """
 from __future__ import unicode_literals
 import requests
@@ -18,12 +19,10 @@ base_page_id = 1049704
 url = 'http://www.xxbiquge.com/1_1413/{}.html'
 
 
-def open_chap(chapter_id):
-    """
-    第chapter_id章内容
-    :param chapter_id: 章节ID，从1开始
-    :return:
-    """
+def get_content(chapter_id):
+    if chapter_id < 2 or chapter_id > 1646:
+        print "章节ID, 从2开始, 1646截止"
+        return
     diff = chapter_id - base_chapter_id
     page_id = base_page_id + diff
     print page_id
@@ -40,6 +39,16 @@ def open_chap(chapter_id):
         elif isinstance(line, Tag):
             if line.text:
                 contents.append(line.text)
+    return contents
+
+
+def open_chap(chapter_id):
+    """
+    第chapter_id章内容
+    :param chapter_id: 章节ID, 从2开始, 1646截止
+    :return:
+    """
+    contents = get_content(chapter_id)
     result = format_show(contents)
     for line in result:
         print line
@@ -62,4 +71,4 @@ def format_show(contents, char_num=20):
 
 
 if __name__ == '__main__':
-    open_chap(1600)
+    open_chap(2)
